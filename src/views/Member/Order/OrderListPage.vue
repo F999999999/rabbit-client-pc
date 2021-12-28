d6e2f71cebf605c279f9cdd2640cadd30a14143d
F999999999 <9811@88.com>
2021-12-28 08:52:12 +0800

    完成：订单管理-渲染订单管理标题

--- src/views/Member/Order/OrderListPage.vue
+++ src/views/Member/Order/OrderListPage.vue
@@ -1,15 +1,28 @@
 <template>
   <AppMemberLayout>
-    <div class="member-order">订单列表</div>
+    <div class="member-order">
+      <XtxTabs v-model:active="active">
+        <XtxTabTitle v-for="status in orderStatus" :key="status.name">
+          {{ status.label }}
+        </XtxTabTitle>
+      </XtxTabs>
+    </div>
   </AppMemberLayout>
 </template>
 
 <script>
 import AppMemberLayout from "@/components/AppMemberLayout";
+import { ref } from "vue";
+import { orderStatus } from "@/api/constants";
 
 export default {
   name: "OrderListPage.vue",
   components: { AppMemberLayout },
+  setup() {
+    const active = ref(0);
+
+    return { orderStatus, active };
+  },
 };
 </script>
 
